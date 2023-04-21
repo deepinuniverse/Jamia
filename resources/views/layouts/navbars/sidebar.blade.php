760a55140a89d1202df197417447833096b9d0a6
ha215 <123815210+ha215@users.noreply.github.com>
2023-04-21 17:11:05 +0300

    new

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,4 +1,18 @@
-<div class="sidebar" data-color="orange" data-background-color="white" data-image="{{ asset('material') }}/img/sidebar-1.jpg">
+ <style>
+      .circle {
+        border-radius: 50%;
+        width: 10px;
+        height: 5px;
+        padding: 5px;
+        background: #fff;
+        border: 1px solid #000;
+        color: #000;
+        text-align: center;
+        font: 12px Arial, sans-serif;
+        border-color: red;
+        color: white;
+      }
+    </style><div class="sidebar" data-color="orange" data-background-color="white" data-image="{{ asset('material') }}/img/sidebar-1.jpg">
   <!--
       Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
@@ -42,31 +56,33 @@
           </ul>
         </div>
       </li> --}}
-      <li class="nav-item{{ $activePage == 'user' ? ' active' : '' }}" >
-        <a class="nav-link" href="/user/list">
+      @if(Auth::user()->userpermission(2))
+      <li class="nav-item{{ $activePage == 'coupon_user' ? ' active' : '' }}" >
+        <a class="nav-link" href="/coupon_user">
           <i class="material-icons">content_paste</i>
             <p>{{ __('jamia.user') }}</p>
         </a>
       </li>
-      <li class="nav-item{{ $activePage == 'coupon_user' ? ' active' : '' }}" >
-        <a class="nav-link" href="/coupon_user">
+      <li class="nav-item{{ $activePage == 'family_card' ? ' active' : '' }}" >
+        <a class="nav-link" href="/family_card">
+          <i class="material-icons">content_paste</i>
+            <p>{{ __('jamia.user') }}</p>
+        </a>
+      </li>
+      @endif
+      @if(Auth::user()->userpermission(14))
+      <li class="nav-item{{ $activePage == 'user' ? ' active' : '' }}" >
+        <a class="nav-link" href="/user/list">
           <i class="material-icons">content_paste</i>
             <p>{{ __('jamia.emp') }}</p>
         </a>
       </li>
+      @endif
       @if(Auth::user()->userpermission(3))
       <li class="nav-item{{ $activePage == 'role' ? ' active' : '' }}" >
         <a class="nav-link" href="/roles">
           <i class="material-icons">content_paste</i>
             <p>{{ __('jamia.role') }}</p>
-        </a>
-      </li>
-      @endif
-      @if(Auth::user()->userpermission(2))
-      <li class="nav-item{{ $activePage == 'user' ? ' active' : '' }}" >
-        <a class="nav-link" href="/user/list">
-          <i class="material-icons">content_paste</i>
-            <p>{{ __('jamia.user') }}</p>
         </a>
       </li>
       @endif
@@ -78,66 +94,112 @@
         </a>
       </li>
       @endif
+      @if(Auth::user()->userpermission(4))
       <li class="nav-item{{ $activePage == 'news' ? ' active' : '' }}" >
         <a class="nav-link" href="/news">
           <i class="material-icons">content_paste</i>
             <p>{{ __('jamia.news_details') }}</p>
         </a>
       </li>
+      @endif
+      @if(Auth::user()->userpermission(5))
       <li class="nav-item{{ $activePage == 'offer' ? ' active' : '' }}" >
         <a class="nav-link" href="/offers">
           <i class="material-icons">content_paste</i>
             <p>{{ __('jamia.offer') }}</p>
         </a>
       </li>
+      @endif
+      @if(Auth::user()->userpermission(6))
       <li class="nav-item{{ $activePage == 'branch' ? ' active' : '' }}" >
         <a class="nav-link" href="/branch_category">
           <i class="material-icons">content_paste</i>
             <p>{{ __('jamia.branch') }}</p>
         </a>
       </li>
+      @endif
+      @if(Auth::user()->userpermission(7))
       <li class="nav-item{{ $activePage == 'branches' ? ' active' : '' }}" >
         <a class="nav-link" href="/branches">
           <i class="material-icons">content_paste</i>
             <p>{{ __('jamia.brch') }}</p>
         </a>
       </li>
+      @endif
+      @if(Auth::user()->userpermission(8))
       <li class="nav-item{{ $activePage == 'discardReport' ? ' active' : '' }}" >
         <a class="nav-link" href="/discard_report">
           <i class="material-icons">content_paste</i>
-            <p>{{ __('jamia.discard') }}</p>
+            <p style="display: inline;">{{ __('jamia.discard') }}</p>
+            @if($discardCount > 0)<span class="notification circle" style="background: red;margin-left: 17px;">{{$discardCount}}</span>@endif
         </a>
       </li>
+      @endif
+      @if(Auth::user()->userpermission(9))
       <li class="nav-item{{ $activePage == 'offerCat' ? ' active' : '' }}" >
         <a class="nav-link" href="/offer_category">
           <i class="material-icons">content_paste</i>
-            <p>{{ __('jamia.offer_cat') }}</p>
+            <p>{{ __('jamia.offer_cat') }} </p>
         </a>
       </li>
+      @endif
+      @if(Auth::user()->userpermission(10))
       <li class="nav-item{{ $activePage == 'couponoffer' ? ' active' : '' }}" >
         <a class="nav-link" href="/coupon_offer">
           <i class="material-icons">content_paste</i>
             <p>{{ __('jamia.coupon_offer') }}</p>
         </a>
       </li>
+      @endif
+      @if(Auth::user()->userpermission(11))
       <li class="nav-item{{ $activePage == 'complaint' ? ' active' : '' }}" >
         <a class="nav-link" href="/complaints">
           <i class="material-icons">content_paste</i>
-            <p>{{ __('jamia.complaint') }}</p>
+            <p style="display: inline;">{{ __('jamia.complaint') }} </p>
+            @if($complaintCount > 0)<span class="notification circle" style="background: red;margin-left: 17px; display: inline;">{{$complaintCount}}</span>@endif
         </a>
       </li>
+      @endif
+      @if(Auth::user()->userpermission(12))
       <li class="nav-item{{ $activePage == 'gallery' ? ' active' : '' }}" >
         <a class="nav-link" href="/galleries">
           <i class="material-icons">content_paste</i>
             <p>{{ __('jamia.gallery') }}</p>
         </a>
       </li>
+      @endif
+      @if(Auth::user()->userpermission(13))
       <li class="nav-item{{ $activePage == 'medias' ? ' active' : '' }}" >
         <a class="nav-link" href="/medias">
           <i class="material-icons">content_paste</i>
             <p>{{ __('jamia.social') }}</p>
         </a>
       </li>
+      @endif
+      @if(Auth::user()->userpermission(15))
+      <li class="nav-item{{ $activePage == 'notification' ? ' active' : '' }}" >
+        <a class="nav-link" href="/notifications">
+          <i class="material-icons">content_paste</i>
+            <p>{{ __('jamia.notification') }}</p>
+        </a>
+      </li>
+      @endif
+      @if(Auth::user()->userpermission(16))
+      <li class="nav-item{{ $activePage == 'slideshows' ? ' active' : '' }}" >
+        <a class="nav-link" href="/slideshows">
+          <i class="material-icons">content_paste</i>
+            <p>{{ __('jamia.slideshows') }}</p>
+        </a>
+      </li>
+      @endif
+      @if(Auth::user()->userpermission(17))
+      <li class="nav-item{{ $activePage == 'slideshows' ? ' active' : '' }}" >
+        <a class="nav-link" href="/informations">
+          <i class="material-icons">content_paste</i>
+            <p>{{ __('jamia.information') }}</p>
+        </a>
+      </li>
+      @endif
     </ul>
   </div>
 </div>
